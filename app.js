533a3c1537c52b10a33234e2b13326712dfd28e8
ParkerPo <b00705048@ntu.edu.tw>
2015-07-06 15:51:32 +0800

    name,rating,price now,price was

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ var cheerio = require('cheerio');
 
 request.get("http://www.whitehouseblackmarket.com/store/category/sale-up-to-70-off/cat9159276",function (err,res,body){
 	$ = cheerio.load(body);
+	var rating=[];
 	var names=[];
+	var price_now=[];
+	var price_was=[];
 //	var counter=1;
-	$('div.product-capsule a.product-name').each(function(i,item){
+	$('a.product-name').each(function(i,item){
 		names[i]=$(this).clone().children().remove().end().text().trim();
 		//console.log(names[i]);
+		rating[i]=$(this).children('.BVRRRatingNormalOutOf').children('.BVRRRatingNumber').text()
+		var price=$(this).parent().children('div.product-price')
+		price_now[i]=parseInt(price.children('span.product-price-now').text().split(/TWD/)[1].replace(/,/i,""))
+		price_was[i]=parseInt(price.children('span.product-price-was').text().split(/TWD/)[1].replace(/,/i,""))
 	});
 
-	console.log(names)
+	for (var i=0;i<names.length;i++){
+		console.log(names[i]+"\t"+rating[i]+"\t"+price_now[i]+"\t"+price_was[i])
+	}
 });
